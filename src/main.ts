3cd812f0df0cce4622562d31f1999f7d7a388598
AlsoPeters <Kyle.fire@gmail.com>
2023-02-01 09:00:58 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import Phaser from 'phaser';
+
+import Preloader from './scenes/Preloader';
+
+import Game from './scenes/Game';
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  parent: 'app',
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+    },
+  },
+  scene: [Preloader, Game],
+  scale: {
+    zoom: 2,
+  },
+};
+
+export default new Phaser.Game(config);
