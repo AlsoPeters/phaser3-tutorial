9994f8ec25660e8b60a9f9ba405124b6e242974f
AlsoPeters <kyle.fire@gmail.com>
2023-02-04 09:11:34 +0900

    add character sprites to game

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,11 +1,14 @@
 import Phaser from 'phaser';
 
 export default class Game extends Phaser.Scene {
+  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
   constructor() {
     super('game');
   }
 
-  preload() {}
+  preload() {
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
 
   create() {
     const map = this.make.tilemap({ key: 'dungeon' });
@@ -22,5 +25,38 @@ export default class Game extends Phaser.Scene {
       collidingTileColor: new Phaser.Display.Color(243, 234, 48, 255),
       faceColor: new Phaser.Display.Color(40, 39, 37, 255),
     });
+
+    const player = this.add.sprite(
+      128,
+      128,
+      'player',
+      'adventurer-idle-00.png'
+    );
+
+    this.anims.create({
+      key: 'player-idle',
+      frames: this.anims.generateFrameNames('player', {
+        start: 0,
+        end: 5,
+        prefix: 'adventurer-idle-0',
+        suffix: '.png',
+      }),
+      repeat: -1,
+      frameRate: 7,
+    });
+
+    this.anims.create({
+      key: 'player-run',
+      frames: this.anims.generateFrameNames('player', {
+        start: 0,
+        end: 5,
+        prefix: 'adventurer-run-0',
+        suffix: '.png',
+      }),
+      repeat: -1,
+      frameRate: 10,
+    });
+
+    player.anims.play('player-idle');
   }
 }
--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -8,6 +8,8 @@ export default class Preloader extends Phaser.Scene {
   preload() {
     this.load.image('tiles', 'tiles/dungeon_tiles.png');
     this.load.tilemapTiledJSON('dungeon', 'tiles/dungeon-01.json');
+
+    this.load.atlas('player', 'character/player.png', 'character/player.json');
   }
 
   create() {
