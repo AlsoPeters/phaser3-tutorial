205b8e99478cc120f1b6025996b64f6cf8fabc86
AlsoPeters <kyle.fire@gmail.com>
2023-02-04 11:47:12 +0900

    add player movement and collision

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ const config: Phaser.Types.Core.GameConfig = {
     default: 'arcade',
     arcade: {
       gravity: { y: 0 },
+      debug: true,
     },
   },
   scene: [Preloader, Game],
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -2,6 +2,8 @@ import Phaser from 'phaser';
 
 export default class Game extends Phaser.Scene {
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+  private player!: Phaser.Physics.Arcade.Sprite;
+
   constructor() {
     super('game');
   }
@@ -26,7 +28,7 @@ export default class Game extends Phaser.Scene {
       faceColor: new Phaser.Display.Color(40, 39, 37, 255),
     });
 
-    const player = this.add.sprite(
+    this.player = this.physics.add.sprite(
       128,
       128,
       'player',
@@ -57,6 +59,44 @@ export default class Game extends Phaser.Scene {
       frameRate: 10,
     });
 
-    player.anims.play('player-idle');
+    this.player.anims.play('player-idle');
+
+    this.physics.add.collider(this.player, wallsLayer);
+    this.player.body.setSize((this.player.width = 32));
+  }
+
+  update(time: number, delta: number): void {
+    if (!this.cursors || !this.player) {
+      return;
+    }
+
+    const speed = 100;
+
+    if (this.cursors.left?.isDown) {
+      this.player.anims.play('player-run', true);
+      this.player.setVelocity(-speed, 0);
+      this.player.scaleX = -1;
+      this.player.body.offset.x = 36;
+    } else if (this.cursors.right?.isDown) {
+      this.player.anims.play('player-run', true);
+      this.player.setVelocity(speed, 0);
+      this.player.scaleX = 1;
+      this.player.body.offset.x = 0;
+    } else if (this.cursors.up?.isDown) {
+      this.player.anims.play('player-run', true);
+      this.player.setVelocity(0, -speed);
+    } else if (this.cursors.down?.isDown) {
+      this.player.anims.play('player-run', true);
+      this.player.setVelocity(0, speed);
+    } else {
+      const parts = this.player.anims.currentAnim.key.split('-');
+      parts[1] = 'idle';
+
+      this.player.play(parts.join('-'));
+      this.player.setVelocity(0, 0);
+      this.cameras.main.startFollow(this.player, true);
+    }
   }
 }
+
+// vid: 30:00
